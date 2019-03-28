f053dc7c2e8d14daf89c45458eb9f844917b5ae0
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2019-03-28 18:02:53 +0000

    Tidy up

--- imageToText/ProcessBasalApplications.py
+++ imageToText/ProcessBasalApplications.py
@@ -18,19 +18,11 @@ class CropSection:
         
 def newCrop(line):
     global crops
-    print("test line for crops: " + line)
     for crop in crops:
-        print("crop occ: " + crop + ", " + str(line.find(crop))) 
         lline = line.lower()
         lcrop = crop.lower()
         if lline.find(lcrop) >= 0 and lline.find(lcrop) <= 5: # look for the crop near the start of the line (first 5 chars)
-        #if line.lower().startswith(crop.lower()):
-            print("got crop : " + crop)
             cropSection = CropSection(crop,line[len(crop)+1:].strip())
-            #cropSection.cropName(crop)
-            #print("====>crop: " + crop)
-            #print("====>line: " + cropSection.data)
-            #cropSection.data()
             return cropSection
     return None
 
@@ -47,14 +39,10 @@ def processCropSections(page):
                 if cropSection:
                     cropSections.append(cropSection)
                 cropSection = newCropSection
-                print("X")
             elif cropSection:
-                print("Y")
                 cropSection.data = " ".join([cropSection.data,line]) 
             else:
-                print("Z")
                 cropSection = CropSection("all crops",line)
-    #print(cropSection.cropName + " - [" + cropSection.data + "]")
     cropSections.append(cropSection)
     return cropSections
 
@@ -66,26 +54,19 @@ def loopDocs():
     year = ""
     fileList = os.listdir(srcdocs)
     fileList.sort()
-    corrections = []
     sections = ["weedkiller:","fungicide:","insecticide:","manures:","weedkillers:"]
-    
-    with open("D:\\Work\\rothamsted-ecoinformatics\\YieldbookDatasetDrafts\\basalCorrections.csv", 'r') as infile:
-        for line in infile:
-            corrections.append(line.strip())
     
     for idx, fname in enumerate(fileList):
         nyear = fname[0:4]
         
-        print("idx: " + str(idx) + ":  nyear = " + nyear + ", year =  " + year)
         if int(nyear) >= 1968 and int(nyear) <= 1991 and fname.endswith(".jpg"): 
             page = getPageScan(srcdocs + "\\" + fname)
             print("RAW PAGE:::::::::::::::::")
             print(page)
             print("\RAW PAGE:::::::::::::::::")
             
-            #page = correctWords(page.replace("\n"," ").split(" "),corrections)
             page = page.replace("\n"," $$ $$ ") # This trick is for retaining line breaks, while allowing for testing line break joined words...
-            page = correctWords(page.split(" "),corrections)
+            page = correctWords(page.split(" "))
             page = page.replace(" $$ $$ ","\n")
             print("CORRECTED PAGE:::::::::::::::::")
             print(page)
@@ -97,28 +78,16 @@ def loopDocs():
                 cutStart = 0
                 cutEnd = len(page)-1 
                 data = True
-                #npage = ""
+                
                 if page.find("Basal applications") >-1:
                     cutStart = page.find("Basal applications")+19
-                    print("a")
-                    #npage = page[page.find("Basal applications")+19:] # this should stop everything before the Cultivations from being processed
-                    #print(npage)
                 elif page.find("Standard applications") >-1:
                     cutStart = page.find("Standard applications")+22
-                    #npage = page[page.find("Standard applications")+22:]
-                    print("b")
-                    #print(npage)
-                #fpage = npage
                 if page.find("Cultivations, etc") > -1:
-                    print("d")
                     cutEnd = page.find("Cultivations, etc")
-                    #fpage = npage[:npage.find("Cultivations")]
                 elif page.find("Seed") > -1:
                     cutEnd = page.find("Seed")
-                    print("c")
-                    #fpage = npage[:npage.find("Seed")]
                 
-                print(nyear + " : " + str(cutStart) + " : " + str(cutEnd) + " : " + str(len(page)))
                 fpage = str(page)[cutStart:cutEnd]
                 fpage = fpage.strip()
             print("=====================DATA=========================")    
@@ -143,33 +112,6 @@ def loopDocs():
                             applicationText = " ".join([applicationText,word.strip()])
                     outfile.write("|".join([str(experiment),str(nyear),cropSection.cropName,application,applicationText.strip()]))
                     outfile.write("\n")
-                    
-                    #print(data)
-                
-                    #section = ""
-                    #for idx2, part in enumerate(applicationData):
-                    #    sectionParts = part.split(" ")
-                    #    lastWord = sectionParts[len(sectionParts)-1].strip()
-                    #    print("(" + lastWord + ")")
-                    #    oldSection = section
-                    #    if lastWord in sections:
-                        #if part[len(part)-1].lower() in sections:
-                            #if len(prevLine) > 0:
-                            #    outfile.write(str(nyear) + "|" + section + "|" + part) 
-                            #    outfile.write("\n")
-                    #        section = lastWord
-                            #part = " ".join(sectionParts[:len(sectionParts)-2])
-                    #    if len(section) > 0 and idx2 > 0:
-                     #       part.replace(cropSection.cropName,"")
-                      #      outfile.write("|".join([str(experiment),str(nyear),cropSection.cropName,oldSection,str(part)]))
-                       #     outfile.write("\n")
-                            
-                    
-                #outfile.write(str(nyear) + "|" + section + "|" + part) 
-                #outfile.write("\n")
-                #page = page.replace("\n"," ")
-                #outfile.write(str(nyear) + ": " + page) 
-                #outfile.write("\n")   
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -177,6 +119,4 @@ experiment = config['EXPERIMENT']['name']
 outfile = open(config['EXPERIMENT']['outfile'], "w+", 1)
 srcdocs = config['EXPERIMENT']['srcdocs']
 crops = config['EXPERIMENT']['crops'].split(",")
-print(crops)
 loopDocs()
-#loopDocs("D:\\work\\yieldbooks\\Exhaustion\\")
