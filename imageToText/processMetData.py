ad126103cd541673648b11379cbef73de2b18556
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2019-07-29 10:05:42 +0100

    Saxmundham metdata updates

--- imageToText/processMetData.py
+++ imageToText/processMetData.py
@@ -12,24 +12,27 @@ from fuzzywuzzy import fuzz
 config = configparser.ConfigParser()
 config.read('config.ini')
 #experiment = config['EXPERIMENT']['name']
-outfile = open(config['EXPERIMENT']['outfile'], "w+", 1)
+#outfile = open(config['EXPERIMENT']['outfile'], "w+", 1)
 srcdocs = config['EXPERIMENT']['srcdocs']
 
 fileList = os.listdir(srcdocs)
 fileList.sort()
 treatments = []
 for fname in fileList:
-    #nyear = fname[0:4]
+    nyear = fname[12:16]
     #npage = fname[4:6]
     #print (nyear + " - " + npage)
     print(srcdocs + "\\" + fname)
     
     if fname.endswith("jpg"):
+        outfile = open(srcdocs + "/"+nyear+".txt", "w+", 1)
         page = getPageScan(srcdocs + "\\" + fname)
         lines = page.split("\n")
         for line in lines:
             data = line.split(" ")
+            outfile.write(nyear)
             for d in data:
-                outfile.write(d + ",")
+                d = d.replace("{","(").replace("}",")").replace("(,","(")
+                outfile.write("," + d)
             outfile.write("\n")
         print(page)
