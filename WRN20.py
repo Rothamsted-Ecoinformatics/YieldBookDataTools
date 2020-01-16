f054de7c2e4657004bcac11ce236079c0d59c64b
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2020-01-16 14:14:54 +0000

    WRN20 data extraction code

--- WRN20.py
+++ WRN20.py
@@ -22,23 +22,32 @@ import os
 #    scan = pytesseract.image_to_string(img, lang='eng', config='--dpi 600 --psm 6',nice=0,output_type=Output.DICT)
 #    page = scan.get("text")
 #    return page
-fname = "D:\\Work\\rothamsted-ecoinformatics\\WRN20\\scan.png"
+fname = "D:\\Work\\rothamsted-ecoinformatics\\WRN20\\Scanplots.png"
 
 img = cv2.imread(fname)
-#img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+#img = cv2.blur(img,(3,3)) # OK
+#img = cv2.medianBlur(img,3)
+img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+kernel = np.ones((3,3),np.uint8)
+img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+#cv2.imshow("pic",img)
+
+#
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-#img = cv2.medianBlur(img,3)
 #kernel = np.ones((1, 1), np.uint8)
 #img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 #img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 #print(img)
-#scan = pytesseract.image_to_string(img, lang='eng', config='--dpi 600 --psm 6',nice=0,output_type=Output.DICT)
+#scan = pytesseract.image_to_string(img, lang='eng', config='--dpi 600 --psm 6 --oem 3 -c tessedit_char_whitelist=0123456789.')
 #scan = pytesseract.image_to_string(img,config='-c preserve_interword_spaces=1x1 --dpi 1200 --psm 6 --oem 3')
+scan = pytesseract.image_to_string(img, lang='eng', config='--dpi 300 --psm 6',nice=0,output_type=Output.DICT)
+   
 #page = scan.get("text")
-#print(page)
+print(scan)
 
-hocr = pytesseract.image_to_pdf_or_hocr(fname, extension='hocr')
+#hocr = pytesseract.image_to_pdf_or_hocr(fname, extension='hocr')
 
-outfile = open("D:\\Work\\rothamsted-ecoinformatics\\WRN20\\sheet1data1200.txt", "w+", 1)
-outfile.writelines(hocr)
+outfile = open("D:\\Work\\rothamsted-ecoinformatics\\WRN20\\Scanplots.txt", "w+", 1)
+outfile.writelines(scan.get("text"))
